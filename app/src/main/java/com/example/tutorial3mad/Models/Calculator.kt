7519d4827732c1e6f3403c4b34d4e319897c824a
yapabandara <dhanuyapaugc@gmail.com>
2023-12-23 19:28:51 +0530

    create new one

---
+++ app/src/main/java/com/example/tutorial3mad/Models/Calculator.kt
@@ -0,0 +1,12 @@
+package com.example.tutorial3mad.Models
+
+class Calculator(private val number1:Double, private val number2:Double) {
+
+    fun add() :Double{
+        return number1+number2
+    }
+    fun substract() =number1-number2
+    fun multiply() =number1-number2
+    fun divide() =number1-number2
+
+}
